4d7be55624a086999ee6c7d8f90b2c2a18379d83
Stefan Schmid <schmist@gmail.com>
2019-04-22 23:54:42 +0200

    Adds register command stub.

--- src/main/kotlin/com/syncatec/rebu/Main.kt
+++ src/main/kotlin/com/syncatec/rebu/Main.kt
@@ -1,6 +1,8 @@
 package com.syncatec.rebu
 
+import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.NoRunCliktCommand
+import com.github.ajalt.clikt.core.subcommands
 import com.github.ajalt.clikt.parameters.options.versionOption
 
 class RebuCommand(rebu: Rebu) : NoRunCliktCommand(name = rebu.name) {
@@ -10,5 +12,12 @@ class RebuCommand(rebu: Rebu) : NoRunCliktCommand(name = rebu.name) {
     }
 }
 
+class RegisterCommand : CliktCommand(name = "register") {
+
+    override fun run() {
+        echo("register")
+    }
+}
+
 fun main(args: Array<String>) =
-    RebuCommand(RebuImpl()).main(args)
+    RebuCommand(RebuImpl()).subcommands(RegisterCommand()).main(args)
